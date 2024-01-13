1af88df76f22d6adebc8baf165b5e3f81a8f7370
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2024-01-13 18:03:17 +0530

    chore: solve all week-6 challenges

--- week-6/2-use-callback/src/components/Assignment1.jsx
+++ week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,31 +1,58 @@
+/* eslint-disable react/prop-types */
+import { useCallback } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
 export function Assignment1() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    // Your code starts here
-    function handleIncrement() {
-
-    }
-
-    function handleDecrement() {
-        
-    }
-    // Your code ends here
-
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
-        </div>
+  // Your code starts here
+  // Empty dependency-array indicates that function is pure and needn't be recreated for varying states
+  const handleIncrement = useCallback(() => {
+    console.log(
+      `Increment triggered, setting value from ${count} to ${count + 1}...`
     );
-};
+    setCount((count) => count + 1); // We are using this initialization of setCount to ensure we have the most recent value, since this function is only created once (component-mounts)
+  }, []);
 
-const CounterButtons = ({ onIncrement, onDecrement }) => (
+  // Empty dependency-array indicates that function is pure and needn't be recreated for varying states
+  const handleDecrement = useCallback(() => {
+    console.log(
+      `Decrement triggered, setting value from ${count} to ${count - 1}...`
+    );
+    setCount(count - 1);
+  }, [count]); // We are declaring the dependency array here to ensure that the function is recreated upon every change of state, so as to not use the initial count value time and again (since this function is only created once during component-mounting)
+
+  return (
     <div>
-        <button onClick={onIncrement}>Increment</button>
-        <button onClick={onDecrement}>Decrement</button>
+      <p>Count: {count}</p>
+      <CounterButtons
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+      />
     </div>
-);
+  );
+}
+
+const CounterButtons = ({ onIncrement, onDecrement }) => {
+  // Intended for understanding the behaviour better
+  useEffect(() => {
+    console.log(
+      `This is displayed due to onDecrement handler function being recreated`
+    );
+  }, [onDecrement]);
+  useEffect(() => {
+    console.log(
+      `This must be displayed only during component-mount due to onIncrement handler config`
+    );
+  }, [onIncrement]);
+
+  return (
+    <div>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+    </div>
+  );
+};
--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,21 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
+  const inputField = useRef(null); // useRef<HTMLInputElement | null>(null)
 
-    useEffect(() => {
+  useEffect(() => {
+    inputField?.current.focus();
+  }, []);
 
-    }, []);
+  const handleButtonClick = () => {
+    inputField?.current.focus();
+  };
 
-    const handleButtonClick = () => {
-
-    };
-
-    return (
-        <div>
-            <input type="text" placeholder="Enter text here" />
-            <button onClick={handleButtonClick}>Focus Input</button>
-        </div>
-    );
-};
+  return (
+    <div>
+      <input type="text" placeholder="Enter text here" ref={inputField} />
+      <button onClick={handleButtonClick}>Focus Input</button>
+    </div>
+  );
+}
